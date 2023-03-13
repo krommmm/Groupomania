57a90d298e3ba3285135da99072fb9cb77754e02
ThibaultB <102362413+krommmm@users.noreply.github.com>
2023-03-13 14:47:17 +0100

    Update varGlobal.js

--- src/components/varGlobal.js
+++ src/components/varGlobal.js
@@ -2,8 +2,8 @@
 const db = {
   port: 2000,
 };
-//url => 13.38.54.185
+
 
 //Variable global qui permet de rajouter le port (car dans la bdd il n'y a que le nom de l'image)
-const varGlobal = `http://localhost:${db.port}`;
+const varGlobal = `http://13.38.54.185:${db.port}`;
 export default varGlobal;
